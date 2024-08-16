e72be3bd5986b48e0e038853f75ebf0ddc165501
Xiaoyuan Liu <72927467+phil-liu1020@users.noreply.github.com>
2024-08-16 12:56:15 +0100

    renamed fig2 to fig3

--- data_generation_scripts/figure3b_1506.m
+++ data_generation_scripts/figure3b_1506.m
@@ -1,4 +1,4 @@
-% fig 2b (m(0),alpha(0))=(1.5,0.6)
+% fig 3b (m(0),alpha(0))=(1.5,0.6)
 % initialising parameters
 
 A=100; M=1; T=1; Cz=0.6; Cp=0; beta1=1; beta2=1; lambda21=0; lambda12=0; mu=0.0005; delta=0.005; NEVOL=3500; f0=0.002; m0=1.5; alpha0=0.6; 
