0463da57a5e29215a443430e750020f48e25e8e7
Xiaoyuan Liu <72927467+phil-liu1020@users.noreply.github.com>
2024-08-16 14:13:00 +0100

    Create figureS2.m

---
+++ data_generation_scripts/figureS2.m
@@ -0,0 +1,9 @@
+beta1=(0.08:0.08:4);
+
+fusion_env2beta1=zeros(1,length(beta1));
+fusion_env1beta1=zeros(1,length(beta1));
+fusion_env2betaBH=zeros(1,length(beta1));
+fusion_env1betaBH=zeros(1,length(beta1));
+figS15output=zeros(1,length(beta1));
+
+addpath(genpath('Sex_and_Size-main'))
