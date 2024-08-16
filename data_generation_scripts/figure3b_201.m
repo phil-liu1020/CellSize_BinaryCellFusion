cdedcffe461b82bef9c6e991c7b1d118bee40428
Xiaoyuan Liu <72927467+phil-liu1020@users.noreply.github.com>
2024-08-16 12:56:53 +0100

    renamed fig2 to fig3

--- data_generation_scripts/figure3b_201.m
+++ data_generation_scripts/figure3b_201.m
@@ -1,4 +1,4 @@
-% fig 2b (m(0),alpha(0))=(2,0.1)
+% fig 3b (m(0),alpha(0))=(2,0.1)
 % initialising parameters
 
 A=100; M=1; T=1; Cz=0.6; Cp=0; beta1=1; beta2=1; lambda21=0; lambda12=0; mu=0.0005; delta=0.005; NEVOL=3500; f0=0.002; m0=2; alpha0=0.1; 
@@ -18,5 +18,5 @@ cd data_generation_scripts
 
 cd ..
 
-save('Data_files\Fig_2\m_fig2b_201.mat','m');
-save('Data_files\Fig_2\alpha_fig2b_201.mat','alpha');
+save('Data_files\Fig_3\m_fig3b_201.mat','m');
+save('Data_files\Fig_3\alpha_fig3b_201.mat','alpha');
